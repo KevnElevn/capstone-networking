2c6d2899687ab0c4fb8b8dbd666cbb8bd8ca5ae5
KevnElevn <kevinli5370@live.com>
2020-02-24 23:45:08 -0500

    add handshake

--- client.cpp
+++ client.cpp
@@ -2,20 +2,38 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <cstring>
 #include <string>
+#include <cstdlib>
+#include <cmath>
 #define PORT 8080
 
 using namespace std;
 
-int main(int arg, char const *argv[]) {
+string createSynPacket(int seq, int bufferSize, int maxMessageSize);
+//bufferSize and maxMessageSize is exponent to raise 2 by.
+string createAckPacket(int seq, int ack);
+string createRstPacket(int seq, int ack);
+
+int main(int argc, char const *argv[]) {
+  if(argc > 1) {
+    if(atoi(argv[1]) > 25) {
+      //Buffer size > ~33MB
+      cerr << "Buffer too big" << endl;
+      return 1;
+    }
+    if(atoi(argv[2]) > 30) {
+      //Max message size > 1GB
+      cerr << "Max message size too big" << endl;
+      return 2;
+    }
+
+  }
   int sock = 0, valread;
   struct sockaddr_in serv_addr;
-  char const *hello = "Hello from client";
-  char buffer[1024] = {0};
+
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
-    cout << "Socket creation error \n";
+    cerr << "Socket creation error \n";
     return -1;
   }
 
@@ -26,18 +44,91 @@ int main(int arg, char const *argv[]) {
   //sin_port and sin_addr must be in network byte order
   if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
   {
-    cout << "Invalid address/ Address not supported \n";
+    cerr << "Invalid address/ Address not supported \n";
     return -1;
   }
 
   if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
   {
-    cout << "Connection failed \n";
+    cerr << "Connection failed \n";
+    return -1;
   }
-  send(sock, hello, strlen(hello), 0);
-  cout << "Hello message sent\n";
-  valread = read(sock, buffer, 1024);
-  cout << buffer << endl;
+  //Connected
+  srand(time(NULL));
+  char* buffer = new char[32];
+  int sequenceNumber = rand() % 1000000;
+  int acknowledgeNumber = 0;
+  int maxBuffer = 5;
+  int bufferVal = static_cast<int>(pow(2,maxBuffer));
+  int maxMessage = 10;
+  int maxMessageVal = static_cast<int>(pow(2,maxMessage));
+  if(argc > 1) {
+    maxBuffer = atoi(argv[1]);
+    maxMessage = atoi(argv[2]);
+  }
+  string sendStr = createSynPacket(sequenceNumber, maxBuffer, maxMessage);
+  send(sock, sendStr.data(), sendStr.size(), 0);
+  cout << "Sent: " << sendStr << endl;
+  valread = read(sock, buffer, 20);
+  cout << "Received: " << buffer << endl;
+  if(buffer[19] != '\0') {
+    string sendStr = createRstPacket(sequenceNumber, acknowledgeNumber);
+    send(sock, sendStr.data(), sendStr.size(), 0);
+    cout << "Sent: " << sendStr << endl;
+    return -1;
+  }
+  string bufferStr;
+  bufferStr = buffer;
+  acknowledgeNumber = atoi(bufferStr.substr(3,6).data()) + 1;
+  if(atoi(bufferStr.substr(9,6).data()) == ++sequenceNumber) {
+    if(atoi(bufferStr.substr(15,2).data()) != maxBuffer) {
+      maxBuffer = min(maxBuffer, atoi(bufferStr.substr(15,2).data()));
+    }
+    if(atoi(bufferStr.substr(17,2).data()) != maxMessage) {
+      maxMessage = min(maxMessage, atoi(bufferStr.substr(17,2).data()));
+    }
+    sendStr = createAckPacket(sequenceNumber, acknowledgeNumber);
+  }
+  else {
+    sendStr = createRstPacket(sequenceNumber, acknowledgeNumber);
+  }
+  if(sequenceNumber == 1000000)
+    sequenceNumber = 0;
+  delete [] buffer;
+  char* newBuffer = new char[bufferVal];
+  buffer = newBuffer;
+  send(sock, sendStr.data(), sendStr.size(), 0);
+  cout << "Sent: " << sendStr << endl;
+  //End connection if RST
+  if(sendStr[0] == 'R')
+    return -1;
+  valread = read(sock, buffer, bufferVal);
 
   return 0;
+}
+
+string createSynPacket(int seq, int maxBufferSize, int maxMessageSize) {
+  string sequence = to_string(seq);
+  string bufferSizeStr = to_string(maxBufferSize);
+  string messageSizeStr = to_string(maxMessageSize);
+  sequence.insert(sequence.begin(), 6-sequence.size(), '0');
+  bufferSizeStr.insert(bufferSizeStr.begin(), 2-bufferSizeStr.size(), '0');
+  messageSizeStr.insert(messageSizeStr.begin(), 2-messageSizeStr.size(), '0');
+  return "SYN" + sequence + bufferSizeStr + messageSizeStr;
+}
+
+string createAckPacket(int seq, int ack) {
+  string sequence = to_string(seq);
+  string acknowledge = to_string(ack);
+  sequence.insert(sequence.begin(), 6-sequence.size(), '0');
+  acknowledge.insert(acknowledge.begin(), 6-acknowledge.size(), '0');
+  return "ACK" + sequence + acknowledge;
+}
+
+string createRstPacket(int seq, int ack) {
+  string sequence = to_string(seq);
+  string acknowledge = to_string(ack);
+  sequence.insert(sequence.begin(), 6-sequence.size(), '0');
+  acknowledge.insert(acknowledge.begin(), 6-acknowledge.size(), '0');
+  return "RST" + sequence + acknowledge;
 }
--- server.cpp
+++ server.cpp
@@ -2,73 +2,155 @@
 //POSIX API
 #include <unistd.h>
 #include <sys/socket.h>
-#include <stdlib.h>
+#include <cstdlib>
 //Internet address family
 #include <netinet/in.h>
-#include <cstring>
 #include <string>
+#include <cmath>
+#include <ctime>
 #define PORT 8080
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    int server_fd,  //socket descriptor
-        new_socket, //used to accept connection request
-        valread;
-    struct sockaddr_in address;
-    int opt = 1;    //used for setsockopt() to manipulate options for the socket sockfd
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char const *hello = "Hello from server";
+string createSynAckPacket(int seq, int& ack, int bufferSize, int maxMessageSize, string& buff);
+string createRstPacket(int seq, int ack);
 
-    // Create socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    /*
-    socket(domain, type, protocol)
-    domain: integer, communication domain e.g., AF_INET (IPv4 protocol) , AF_INET6 (IPv6 protocol)
-    type: communication type
-        SOCK_STREAM: TCP (reliable, connection oriented)
-        SOCK_DGRAM: UDP(unreliable, connectionless)
-    protocol: Protocol value for IP, which is 0
-    */
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+int main(int argc, char const *argv[]) {
+  if(argc > 1) {
+    if(atoi(argv[1]) > 26) {
+      //Buffer size > ~66MB
+      cerr << "Buffer too big" << endl;
+      return 1;
     }
-    cout << '1' << endl;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-    /*
-    */
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+    if(atoi(argv[2]) > 31) {
+      //Max message size > 2GB
+      cerr << "Max message size too big" << endl;
+      return 2;
     }
+  }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+  int server_fd,  //socket descriptor
+      new_socket, //used to accept connection request
+      valread;
+  struct sockaddr_in address;
+  int opt = 1;    //used for setsockopt() to manipulate options for the socket sockfd
+  int addrlen = sizeof(address);
 
-    //Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0) //listen(socket descriptor, backlog)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read(new_socket, buffer, 1024);
-    cout << buffer << endl;
-    send(new_socket, hello, strlen(hello), 0);
-    cout << "Hello message sent \n";
+  // Create socket file descriptor
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+  /*
+  socket(domain, type, protocol)
+  domain: integer, communication domain e.g., AF_INET (IPv4 protocol) , AF_INET6 (IPv6 protocol)
+  type: communication type
+      SOCK_STREAM: TCP (reliable, connection oriented)
+      SOCK_DGRAM: UDP(unreliable, connectionless)
+  protocol: Protocol value for IP, which is 0
+  */
+  {
+      perror("socket failed");
+      exit(EXIT_FAILURE);
+  }
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+  {
+      perror("setsockopt");
+      exit(EXIT_FAILURE);
+  }
 
-    return 0;
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons( PORT );
+
+  //Forcefully attaching socket to the port 8080
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+  {
+      perror("bind failed");
+      exit(EXIT_FAILURE);
+  }
+  if (listen(server_fd, 3) < 0) //listen(socket descriptor, backlog)
+  {
+      perror("listen");
+      exit(EXIT_FAILURE);
+  }
+  cout << "Listening..." << endl;
+  if((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+  {
+      perror("accept");
+      exit(EXIT_FAILURE);
+  }
+  //Connected and listening
+  char* buffer = new char[32];
+  srand(time(NULL)+1);
+  int sequenceNumber = rand() % 1000000; cout << "Server sequence number: " << sequenceNumber << endl;
+  int acknowledgeNumber = 0;
+  int maxBuffer = 5;
+  int bufferVal = static_cast<int>(pow(2,maxBuffer));
+  int maxMessage = 10;
+  int maxMessageVal = static_cast<int>(pow(2,maxMessage));
+  if(argc > 1) {
+    maxBuffer = atoi(argv[1]);
+    maxMessage = atoi(argv[2]);
+  }
+  valread = read(new_socket, buffer, 14);
+  cout << "Received: " << buffer << endl;
+  if(buffer[13] != '\0') {
+    string sendStr = createRstPacket(sequenceNumber, acknowledgeNumber);
+    send(new_socket, sendStr.data(), sendStr.size(), 0);
+    cout << "Sent: " << sendStr << endl;
+    return -1;
+  }
+  string bufferStr;
+  bufferStr = buffer;
+  if(atoi(bufferStr.substr(9,2).data()) != maxBuffer) {
+    maxBuffer = min(maxBuffer, atoi(bufferStr.substr(9,2).data()));
+  }
+  if(atoi(bufferStr.substr(11,2).data()) != maxMessage) {
+    maxMessage = min(maxMessage, atoi(bufferStr.substr(11,2).data()));
+  }
+  string sendStr = createSynAckPacket(sequenceNumber, acknowledgeNumber, maxBuffer, maxMessage, bufferStr);
+  delete [] buffer;
+  char* newBuffer = new char[bufferVal];
+  buffer = newBuffer;
+  send(new_socket, sendStr.data(), sendStr.size(), 0);
+  cout << "Sent: " << sendStr << endl;
+  valread = read(new_socket, buffer, 16);
+  cout << "Received: "<< buffer << endl;
+  if(buffer[15] != '\0') {
+    string sendStr = createRstPacket(sequenceNumber, acknowledgeNumber);
+    send(new_socket, sendStr.data(), sendStr.size(), 0);
+    cout << "Sent: " << sendStr << endl;
+    return -1;
+  }
+  bufferStr = buffer;
+  if(atoi(bufferStr.substr(9,6).data()) != ++sequenceNumber) {
+    acknowledgeNumber = atoi(bufferStr.substr(3,6).data()) + 1;
+    sendStr = createRstPacket(sequenceNumber, acknowledgeNumber);
+    send(new_socket, sendStr.data(), sendStr.size(), 0);
+    cout << "Sent: " << sendStr << endl;
+    return -1;
+  }
+  return 0;
+}
+
+string createSynAckPacket(int seq, int& ack, int bufferSize, int maxMessageSize, string& buff) {
+  string sequence = to_string(seq);
+  string acknowledge = buff.substr(3,6);
+  string bufferSizeStr = to_string(bufferSize);
+  string messageSizeStr = to_string(maxMessageSize);
+  ack = atoi(acknowledge.data()) + 1;
+  if(ack > 999999)
+    ack = 0;
+  acknowledge = to_string(ack);
+  sequence.insert(sequence.begin(), 6-sequence.size(), '0');
+  acknowledge.insert(acknowledge.begin(), 6-acknowledge.size(), '0');
+  bufferSizeStr.insert(bufferSizeStr.begin(), 2-bufferSizeStr.size(), '0');
+  messageSizeStr.insert(messageSizeStr.begin(), 2-messageSizeStr.size(), '0');
+  return "SAK" + sequence + acknowledge + bufferSizeStr + messageSizeStr;
+}
+
+string createRstPacket(int seq, int ack) {
+  string sequence = to_string(seq);
+  string acknowledge = to_string(ack);
+  sequence.insert(sequence.begin(), 6-sequence.size(), '0');
+  acknowledge.insert(acknowledge.begin(), 6-acknowledge.size(), '0');
+  return "RST" + sequence + acknowledge;
 }
